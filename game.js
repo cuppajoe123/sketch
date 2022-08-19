4e0a2b8ecae07a23834ef13d21854a48b19ca4ed
Joe Strauss <cuppajava@tutanota.com>
2022-08-19 18:17:23 -0500

    Added input for changing dimensions

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.gridContainer');
 
 function createGrid(dimension) {
+    container.replaceChildren();
     for (let i = 0; i < dimension*dimension; i++) {
         let cell = document.createElement('div');
         cell.classList.add('cell');
@@ -15,21 +16,32 @@ function createGrid(dimension) {
     container.style.gridTemplateColumns = gridTemplate;
 }
 
-createGrid(80);
-
-let mouseDown = 0;
-document.body.onmousedown = () => {
-    mouseDown = true;
-}
-document.body.onmouseup = () => {
-    mouseDown = false;
-}
-
-const cells = document.querySelectorAll('.cell');
-cells.forEach((cell) => {
-    cell.addEventListener('pointerenter', () => {
-        if (mouseDown) {
-            cell.style.backgroundColor = 'blue';
-        }
+function registerEventListeners() {
+    let mouseDown = 0;
+    document.body.onmousedown = () => {
+        mouseDown = true;
+    }
+    document.body.onmouseup = () => {
+        mouseDown = false;
+    }
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.addEventListener('pointerenter', () => {
+            if (mouseDown) {
+                cell.style.backgroundColor = 'blue';
+            }
+        });
     });
+}
+
+createGrid(80);
+registerEventListeners();
+
+const submitGridLength = document.querySelector('.submit');
+submitGridLength.addEventListener('click', () => {
+    let gridLength = document.getElementById('inputId').value;
+    if (gridLength > 0 && gridLength <= 100) {
+        createGrid(gridLength);
+        registerEventListeners();
+    }
 });
